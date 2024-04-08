ded7753243ba3176f57fe72ae8d6befb0bab0b08
VedantCars2004 <vgohe2@illinois.edu>
2024-04-07 21:30:36 -0500

    fixed styling issues, template ready

--- src/ProjectsPage.js
+++ src/ProjectsPage.js
@@ -14,24 +14,18 @@ const ProjectsPage = () => {
       <div className="project-boxes">
         <div className="project-box">
           <h3>Project Title 1</h3>
+          <br></br>
           <p>Project description goes here...</p>
-          <p>Technologies used:</p>
-          <ul>
-            <li>Technology 1</li>
-            <li>Technology 2</li>
-          </ul>
+          <br></br>
           <a href="https://github.com/your-username/project-repo-1" target="_blank" rel="noopener noreferrer">
             GitHub Repository
           </a>
         </div>
         <div className="project-box">
           <h3>Project Title 2</h3>
+          <br></br>
           <p>Project description goes here...</p>
-          <p>Technologies used:</p>
-          <ul>
-            <li>Technology 1</li>
-            <li>Technology 2</li>
-          </ul>
+          <br></br>
           <a href="https://github.com/your-username/project-repo-2" target="_blank" rel="noopener noreferrer">
             GitHub Repository
           </a>
